db87faa8007943d4b41039fb626d2b6adbbdea36
wormholecowboy <wormholecowboy@gmail.com>
2022-06-22 22:03:00 -0400

    adding small fn for getting userID

--- app.js
+++ app.js
@@ -7,33 +7,6 @@ const { JWT } = require('google-auth-library');
 const keys = require('./credentials.json');
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
-// async function getAuthToken() {
-//   const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-
-//   const auth = new google.auth.GoogleAuth({
-//     keyFile: './credentials.json',
-//     scopes: SCOPES,
-//     projectId: 'pto-query',
-//   });
-//   const authToken = await auth.getClient();
-//   console.log(authToken);
-//   return authToken;
-// }
-// const client = getAuthToken();
-
-// async function jwtAuth() {
-//   const client = new JWT({
-//     email: keys.client_email,
-//     key: keys.private_key,
-//     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-//   });
-//   const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`;
-//   const res = await client.request({ url });
-//   console.log(res.data);
-// }
-
-// jwtAuth().catch(console.error);
-
 let jwtClient = new google.auth.JWT({
   keyFile: keys,
   email: keys.client_email,
@@ -53,14 +26,12 @@ jwtClient.authorize(function (err, tokens) {
   }
 });
 
-//console.log(jwtClient);
-
 const sheets = google.sheets({ version: 'v4', auth: jwtClient });
 const spreadsheetId = '1ub70x4_NOCzVDLYcH8fmZis1F7nRG87iCLWQ_pnnnP4';
 const range = 'pto-left!A1:B3';
 
-function queryPTO() {
-  let data = sheets.spreadsheets.values.get(
+async function queryPTO(user) {
+  let data = await sheets.spreadsheets.values.get(
     {
       spreadsheetId: spreadsheetId,
       range: range,
@@ -83,7 +54,7 @@ function queryPTO() {
   return data;
 }
 
-// Initializes your app with your bot token and signing secret
+// Slack Auth
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -92,18 +63,18 @@ const app = new App({
   port: process.env.PORT || 3000,
 });
 
-// // Listens to incoming messages that contain "hello"
-// app.message('hello', async ({ message, say }) => {
-//   // say() sends a message to the channel where the event was triggered
-//   await say(`Hey there <@${message.user}>!`);
-// });
+let user;
+function setUser(u) {
+  user = u;
+}
 
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
-  let res = await queryPTO();
-  console.log(res);
-  // say() sends a message to the channel where the event was triggered
-  await say('hiya');
+  //console.log(res);
+  setUser(message.user);
+  console.log(user);
+  //let res = queryPTO();
+  //await say(`${res}`);
 });
 
 (async () => {
